d0f6c14091d2184cdd99a5ea6073e3d18a8566e9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-11-26 11:48:14 +0300

    feat: hide burger bu clicking menu

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -5,11 +5,18 @@ const burgerBtn = document.querySelector(".burger-btn");
 const navList = document.querySelector(".nav-list");
 const burgerLineOne = document.querySelector(".line-one");
 const burgerLineTwo = document.querySelector(".line-two");
+const navItems = document.querySelectorAll(".nav-item");
 
-burgerBtn.addEventListener("click", () => {
+function moveBurgerNav() {
   body.classList.toggle("body-burger");
   navList.classList.toggle("nav-list__shown");
   burgerLineOne.classList.toggle("line-one__crossed");
   burgerLineTwo.classList.toggle("line-two__crossed");
   burgerBtn.classList.toggle("burger-btn__crossed");
+}
+
+burgerBtn.addEventListener("click", moveBurgerNav);
+
+navItems.forEach((elem) => {
+  elem.addEventListener("click", moveBurgerNav);
 });
