afd1063dd997a397c37fad0272e694ccb9f7adb9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-11-26 13:42:16 +0300

    feat: add calculate data

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -6,6 +6,10 @@ const navList = document.querySelector(".nav-list");
 const burgerLineOne = document.querySelector(".line-one");
 const burgerLineTwo = document.querySelector(".line-two");
 const navItems = document.querySelectorAll(".nav-item");
+const daysNumber = document.querySelector(".days-number");
+const hourNumber = document.querySelector(".hours-number");
+const minNumber = document.querySelector(".min-number");
+const secNumber = document.querySelector(".sec-number");
 
 function moveBurgerNav() {
   body.classList.toggle("body-burger");
@@ -14,6 +18,21 @@ function moveBurgerNav() {
   burgerLineTwo.classList.toggle("line-two__crossed");
   burgerBtn.classList.toggle("burger-btn__crossed");
 }
+
+function calcTimeToNewYear() {
+  const now = new Date();
+  const NewYear = new Date("2025-01-01");
+  NewYear.setUTCFullYear(2025, 0, 1);
+  const timeRemainder = NewYear - now;
+  const result = { days: 0, hours: 0, min: 0, sec: 0 };
+  result.days = Math.floor(timeRemainder / 1000 / 60 / 60 / 24);
+  result.hours = Math.floor((timeRemainder / 1000 / 60 / 60) % 24);
+  result.min = Math.floor((timeRemainder / 1000 / 60) % 60);
+  result.sec = Math.ceil((timeRemainder / 1000) % 60);
+  return result;
+}
+
+console.log(calcTimeToNewYear());
 
 burgerBtn.addEventListener("click", moveBurgerNav);
 
@@ -33,3 +52,7 @@ addEventListener("resize", (event) => {
     burgerBtn.classList.remove("burger-btn__crossed");
   }
 });
+
+let timer = setInterval(function tick() {
+  console.log(calcTimeToNewYear());
+}, 200);
