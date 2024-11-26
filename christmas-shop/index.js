fd9e6976c162641ca16bf95ee4e8c92ee017fb5c
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-11-26 11:30:36 +0300

    feat: add appearance of burger

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -1,1 +1,14 @@
 import { giftList } from "./storage.js";
+
+const body = document.querySelector("body");
+const burgerBtn = document.querySelector(".burger-btn");
+const navList = document.querySelector(".nav-list");
+const burgerLineOne = document.querySelector(".line-one");
+const burgerLineTwo = document.querySelector(".line-two");
+
+burgerBtn.addEventListener("click", () => {
+  body.classList.toggle("body-burger");
+  navList.classList.toggle("nav-list__shown");
+  burgerLineOne.classList.toggle("line-one__crossed");
+  burgerLineTwo.classList.toggle("line-two__crossed");
+});
